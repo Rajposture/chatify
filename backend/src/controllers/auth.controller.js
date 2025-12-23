8f5b01e17e3b9ebeb1f389f6ab7fd32961089456
Raj Posture <rajposture16@gmail.com>
2025-12-23 11:54:14 +0530

    changes

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,26 +1,23 @@
-export const signup = async(req,res) =>{
-  const {fullname ,email,password} = req.body;
- try{
-if(!fullname || !email || !password){
-    return res.status (400).json ({MessageChannel:"all fileds are required"})
-}
-if(password.length < 6){
-    return res.status (400).json ({MessageChannel: "password must be at least 6 characters long"})
-}
-const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-if(!emailRegex.test(email)){
-    return res.status (400).json ({MessageChannel:"invalid email format"});
-}
+export const signup = async (req, res) => {
+  const { fullname, email, password } = req.body;
+  try {
+    if (!fullname || !email || !password) {
+      return res
+        .status(400)
+        .json({ MessageChannel: "all fileds are required" });
+    }
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({
+          MessageChannel: "password must be at least 6 characters long",
+        });
+    }
+    const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ MessageChannel: "invalid email format" });
+    }
 
-
-const user
- }catch(error){
-
- }
-
-
-
-
-
-
-}
+    const user = await User.findOne({ email });
+  } catch (error) {}
+};
